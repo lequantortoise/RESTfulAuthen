77b005c5f06e86eb56ad4ee032e480449c3e3c9e
quan <quanska90@gmail.com>
2021-12-08 22:21:54 +0700

    First commit

---
+++ app.js
@@ -0,0 +1,80 @@
+require("dotenv").config();
+const express = require("express");
+const bodyPaser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+app.use(bodyPaser.urlencoded({extended: true}));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser:true});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+// const secret = "Thisisourlittlesecret"; //This line had moved to .env
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields:['password']});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req, res){
+  res.render("home");
+});
+
+app.get("/login", function(req, res){
+  res.render("login");
+});
+
+app.get("/register", function(req, res){
+  res.render("register");
+});
+
+app.post("/register", function(req, res){
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(e){
+    if(e){
+      console.log(e);
+    }else{
+      res.render("secrets");
+    }
+  });
+});
+
+app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = req.body.password;
+
+  User.findOne({email:username}, function(e, foundUser){
+    if(e){
+      console.log(e);
+    }else{
+      if(foundUser){
+        if(foundUser.password === password){
+          res.render("secrets");
+        }
+      }
+    }
+    res.render("login");
+  });
+});
+
+
+
+
+
+
+
+
+
+app.listen(process.env.PORT || 3000, function(req, res){
+  console.log("Server is running ...");
+});
